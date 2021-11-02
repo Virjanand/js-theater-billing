0cd4ed73b5c1226b529f671d62e05c9709e90f75
Virjanand <virjanand@virjanand.com>
2021-11-02 13:13:17 +0100

    add test that covers all flows

---
+++ statement.test.js
@@ -0,0 +1,15 @@
+const statement = require('./statement');
+const invoices = require('./invoices.json')
+const plays = require('./plays.json')
+
+test('print invoice of plays', () => {
+    expect(statement(invoices[0], plays)).toBe(
+`Statement for BigCo
+  Hamlet: $650.00 (55 seats)
+  As You Like It: $580.00 (35 seats)
+  Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
+`
+    );
+});
