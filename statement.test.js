a363d5a0171643ddccf60b37caaa6537f4fdcf2b
Virjanand <virjanand@virjanand.com>
2021-11-02 13:14:59 +0100

    Sum example no longer needed

--- statement.test.js
+++ statement.test.js
@@ -7,9 +7,10 @@ test('print invoice of plays', () => {
 `Statement for BigCo
   Hamlet: $650.00 (55 seats)
   As You Like It: $580.00 (35 seats)
-  Othello: $500.00 (40 seats)
-Amount owed is $1,730.00
-You earned 47 credits
+  Othello: $400.00 (30 seats)
+  As You Like It: $360.00 (20 seats)
+Amount owed is $1,990.00
+You earned 41 credits
 `
     );
 });
