834ec6766b17d9db57f2052cf48c6d63b95b573e
Virjanand <virjanand@virjanand.com>
2021-11-03 15:43:19 +0100

    Inline variable totalVolume

--- statement.js
+++ statement.js
@@ -2,14 +2,13 @@ function statement(invoice, plays) {
     let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
-    let volumeCredits = totalVolumeCredits();
     for (let perf of invoice.performances) {
         // print line for this order
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
     result += `Amount owed is ${usd(totalAmount)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
 
