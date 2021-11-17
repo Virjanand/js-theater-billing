f4018bf0b88d9517beacce3a4f84cc93a00ee5b6
Virjanand <virjanand@virjanand.com>
2021-11-17 15:57:40 +0100

    Add htmlStatement to print statement in html format

--- src/statement.test.js
+++ src/statement.test.js
@@ -1,8 +1,8 @@
-const statement = require('./statement');
-const invoices = require('./testdata/invoices.json');
-const plays = require('./testdata/plays.json');
+const { statement, htmlStatement } = require('./statement')
+const invoices = require('./testdata/invoices.json')
+const plays = require('./testdata/plays.json')
 
-test('print invoice of plays', () => {
+test('text invoice of plays', () => {
   expect(statement(invoices[0], plays)).toBe(
     `Statement for BigCo
   Hamlet: $650.00 (55 seats)
@@ -12,5 +12,20 @@ test('print invoice of plays', () => {
 Amount owed is $1,990.00
 You earned 41 credits
 `,
-  );
-});
+  )
+})
+
+test('html invoice of plays', () => {
+  expect(htmlStatement(invoices[0], plays)).toBe(
+    `<h1>Statement for BigCo</h1>
+<table>
+<tr><th>plays</th><th>seats</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td$650.00</td></tr>
+  <tr><td>As You Like It</td><td>35</td><td$580.00</td></tr>
+  <tr><td>Othello</td><td>30</td><td$400.00</td></tr>
+  <tr><td>As You Like It</td><td>20</td><td$360.00</td></tr>
+</table>
+<p>Amount owed is <em>$1,990.00</em></p>
+<p>You earned <em>41</em> credits</p>
+`,
+  )
+})
