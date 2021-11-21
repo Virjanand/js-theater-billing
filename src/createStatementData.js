5ed1dbee8612be3b6507a865fc0924f953fb9124
Virjanand <virjanand@virjanand.com>
2021-11-21 08:35:50 +0100

    introduce subclasses to replace if statements with polymorphism

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -45,7 +45,10 @@ function createStatementData(invoice, plays) {
   return statementData
 
   function enrichPerformance(aPerformance) {
-    const calculator = createPerformanceCalculator(aPerformance, playFor)
+    const calculator = createPerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance),
+    )
     const result = Object.assign({}, aPerformance)
     result.play = calculator.play
     result.amount = calculator.amount
@@ -66,8 +69,19 @@ function createStatementData(invoice, plays) {
   }
 }
 
-function createPerformanceCalculator(aPerformance, playFor) {
-  return new performanceCalculator(aPerformance, playFor(aPerformance))
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay)
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay)
+    default:
+      throw new Error(`unknown type: ${aPlay.type}`)
+  }
 }
+
+class TragedyCalculator extends performanceCalculator {}
+
+class ComedyCalculator extends performanceCalculator {}
 
 module.exports = createStatementData
